ce78e0d68489e33ed8af12c2c184cea5a14b9074
qrft <lvanderroest1@gmail.com>
2026-09-11 12:52:58 +0000

    Add files via upload

--- public/Prism Commands.lua
+++ public/Prism Commands.lua
@@ -1080,7 +1080,7 @@ registerCommand("vcbypasser", "Bypass voice chat restrictions", {}, function(arg
         VoiceChatService:rejoinVoice()
     end)
 
-    task.wait(0.02)
+    task.wait(0.01)
 
     pcall(function()
         for _, connection in pairs(getconnections(VoiceChatInternal.StateChanged)) do
