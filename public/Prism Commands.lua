ad1989af7392db22ba8014930249234e583a381e
qrft <lvanderroest1@gmail.com>
2026-09-12 09:55:30 +0000

    Add files via upload

--- public/Prism Commands.lua
+++ public/Prism Commands.lua
@@ -1076,12 +1076,17 @@ registerCommand("vcbypasser", "Bypass voice chat restrictions", {}, function(arg
     local VoiceChatService = game:GetService("VoiceChatService")
     local VoiceChatInternal = game:GetService("VoiceChatInternal")
 
-    local Code = 'game:GetService("VoiceChatService"):rejoinVoice()\n'
-        ..'task.wait(0.02)\n'
-        ..'for _, Connections in getconnections(game:GetService("VoiceChatInternal").StateChanged) do\n'
-        ..'    Connections:Disable()\n'
-        ..'end'
-    loadstring(Code)()
+    pcall(function()
+        VoiceChatService:rejoinVoice()
+    end)
+
+    task.wait(0.02)
+
+    pcall(function()
+        for _, connection in pairs(getconnections(VoiceChatInternal.StateChanged)) do
+            connection:Disable()
+        end
+    end)
 
     PM.VCBypasser.active = true
 
